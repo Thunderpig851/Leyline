4519710148e42a403fc56f3607f26e9cd9189bea
Thunderpig851 <87243196+Thunderpig851@users.noreply.github.com>
2026-02-12 15:47:42 -0500

    Repo Setup

---
+++ src/app/router.tsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+import LobbyPage from "../pages/LobbyPage";
+import RoomPage from "../pages/RoomPage";
+import LoginPage from "../pages/LoginPage";
+import NotFoundPage from "../pages/NotFoundPage";
+
+
+export const router = createBrowserRouter([
+  { path: "/", element: <LobbyPage /> },
+  { path: "*", element: <NotFoundPage /> },
+  {path: "/room", element: <RoomPage />},
+  {path: "/login", element: <LoginPage />},
+]);
