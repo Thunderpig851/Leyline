1b2059d127cc3bdec0961bcf70073661ff447494
Thunderpig851 <87243196+Thunderpig851@users.noreply.github.com>
2026-02-16 14:19:34 -0500

    api paths set w/ mongo connection

--- server/src/routes/express.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const express = require('express');
-const { connectToMongoDB } = require('../database/mongo');
-
-const app = express();
-
-// Middleware
-app.use(express.json());
-
